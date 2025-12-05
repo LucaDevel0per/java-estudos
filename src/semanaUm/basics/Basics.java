0b341e64afc1e609b0037d39dcf697c5411f599a
Luca Ribeiro lopes <lucaribeiro1242@gmail.com>
2025-12-05 18:39:04 -0300

    dataTypes

--- src/semanaUm/basics/Basics.java
+++ src/semanaUm/basics/Basics.java
@@ -3,14 +3,6 @@ package semanaUm.basics;
 public class Basics {
     static void main() {
 
-        int idade = 22;
-        double altura = 1.71;
-        char sexo = 'M';
-        boolean ehDoador = false;
-        String nome = "Luca";
-
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
-        System.out.println("Altura: " + altura);
+        System.out.println("Hello World! Let's restart with Java!");
     }
 }
---
+++ src/semanaUm/basics/dataTypes.java
@@ -0,0 +1,32 @@
+package semanaUm.basics;
+
+public class dataTypes {
+    static void main() {
+        // Tipos primitivos
+
+        // byte
+        byte idadeDog = 125; // -128 a 127
+
+        //short
+        short populacaoSuzano = 32767;
+
+        int salario = 5000;
+
+        long populacaoMundial = 800000000000L; // L no final para mostrar que é "LONG"
+
+        float piAproximado = 3.14f; // 'f' no final
+
+        double valorInvestido = 1234.76;
+
+        boolean isTrue = true;
+
+        char letraA = 'A';
+
+        // Tipos de Referencia
+
+        String nome = "Luca";
+
+        int[] numeros = new int[5];
+        String[] carros = {"Fusca", "Ferrari"};
+    }
+}
