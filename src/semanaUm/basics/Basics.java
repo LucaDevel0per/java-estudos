9578d10eaf1dec8649f9a23269dadcde7f2c5820
Luca Ribeiro lopes <lucaribeiro1242@gmail.com>
2025-12-05 18:22:29 -0300

    primeiro commit

---
+++ src/semanaUm/basics/Basics.java
@@ -0,0 +1,16 @@
+package semanaUm.basics;
+
+public class Basics {
+    static void main() {
+
+        int idade = 22;
+        double altura = 1.71;
+        char sexo = 'M';
+        boolean ehDoador = false;
+        String nome = "Luca";
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+    }
+}
